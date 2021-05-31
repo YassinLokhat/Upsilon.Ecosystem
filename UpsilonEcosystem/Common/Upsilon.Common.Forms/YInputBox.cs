f6d4cbad7a8f4e5878ef9d6581ddcba2a258b3a5
yassinlokhat <yassinlokhat@gmail.com>
2021-05-31 16:15:29 +0400

    Upsilon.Common.Forms -  Squeleton

--- UpsilonEcosystem/Common/Upsilon.Common.Forms/YInputBox.cs
+++ UpsilonEcosystem/Common/Upsilon.Common.Forms/YInputBox.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace Upsilon.Common.Forms
 {
     public static class YInputBox
     {
+        public enum YInputType
+        {
+            None = 0,
+            TextBox,
+            Password,
+            Dropdown,
+            ComboBox,
+        }
 
+        public static DialogResult ShowDialog(string title, string message, out string result, YInputType inputType)
+        {
+            result = string.Empty;
+
+            return DialogResult.None;
+        }
     }
 }
