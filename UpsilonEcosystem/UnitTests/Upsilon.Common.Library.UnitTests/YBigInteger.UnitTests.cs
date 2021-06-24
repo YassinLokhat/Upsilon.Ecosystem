409fdd05cce31ccd95f45d3970290088435467a0
yassinlokhat <yassinlokhat@gmail.com>
2021-06-24 10:42:01 +0400

    Upsilon.Common.Library - Adding tests for addition and multiplication of YBigInteger

---
+++ UpsilonEcosystem/UnitTests/Upsilon.Common.Library.UnitTests/YBigInteger.UnitTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upsilon.Common.Library.UnitTests
+{
+    [TestClass]
+    public class YBigInteger_UnitTests
+    {
+        private readonly int LoopCount = 1000;
+
+        [TestMethod]
+        public void Test_01_Sum()
+        {
+            Random random = new((int)DateTime.Now.Ticks);
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                // Given
+                long val1 = random.Next();
+                long val2 = random.Next();
+                YBigInteger number1 = new(BitConverter.GetBytes(val1));
+                YBigInteger number2 = new(BitConverter.GetBytes(val2));
+                YBigInteger number3 = new(BitConverter.GetBytes(val1 + val2));
+
+                // When
+                YBigInteger sum = number1 + number2;
+
+                // Then
+                sum.ByteArray.Should().BeEquivalentTo(number3.ByteArray);
+            }
+        }
+
+        [TestMethod]
+        public void Test_02_Multiply()
+        {
+            Random random = new((int)DateTime.Now.Ticks);
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                // Given
+                long val1 = random.Next();
+                long val2 = random.Next();
+                YBigInteger number1 = new(BitConverter.GetBytes(val1));
+                YBigInteger number2 = new(BitConverter.GetBytes(val2));
+                YBigInteger number3 = new(BitConverter.GetBytes(val1 * val2));
+                
+                // When
+                YBigInteger sum = number1 * number2;
+                
+                // Then
+                sum.ByteArray.Should().BeEquivalentTo(number3.ByteArray);
+            }
+        }
+    }
+}
--- UpsilonEcosystem/Upsilon.Common/Upsilon.Common.Library/YBigInteger.cs
+++ UpsilonEcosystem/Upsilon.Common/Upsilon.Common.Library/YBigInteger.cs
@@ -14,45 +14,50 @@ namespace Upsilon.Common.Library
         public string Prefix { get; set; }
         public int DigitGroup { get; set; }
 
-        public static Base[] Bases { get; } = new Base[]
-        {
-            Base.Binary,
-            Base.Octal,
-            Base.Decimal,
-            Base.Hexadecimal,
-        };
+        public static Base[] Bases { get {
+            return new Base[]
+            {
+                Base.Binary,
+                Base.Octal,
+                Base.Decimal,
+                Base.Hexadecimal,
+            }; } }
 
-        public static Base Binary { get; } = new Base
-        {
-            BaseNumber = 2,
-            Alphabet = "01",
-            Prefix = "0b",
-            DigitGroup = 8,
-        };
+        public static Base Binary { get {
+            return new Base
+            {
+                BaseNumber = 2,
+                Alphabet = "01",
+                Prefix = "0b",
+                DigitGroup = 8,
+            }; } }
 
-        public static Base Octal { get; } = new Base
-        {
-            BaseNumber = 8,
-            Alphabet = "01234567",
-            Prefix = "0o",
-            DigitGroup = 2,
-        };
+        public static Base Octal { get {
+            return new Base
+            {
+                BaseNumber = 8,
+                Alphabet = "01234567",
+                Prefix = "0o",
+                DigitGroup = 2,
+            }; } }
 
-        public static Base Decimal { get; } = new Base
-        {
-            BaseNumber = 10,
-            Alphabet = "0123456789",
-            Prefix = "0d",
-            DigitGroup = 1,
-        };
+        public static Base Decimal { get {
+            return new Base
+            {
+                BaseNumber = 10,
+                Alphabet = "0123456789",
+                Prefix = "0d",
+                DigitGroup = 1,
+            }; } }
 
-        public static Base Hexadecimal { get; } = new Base
-        {
-            BaseNumber = 16,
-            Alphabet = "0123456789ABCDEF",
-            Prefix = "0x",
-            DigitGroup = 2,
-        };
+        public static Base Hexadecimal { get {
+            return new Base
+            {
+                BaseNumber = 16,
+                Alphabet = "0123456789ABCDEF",
+                Prefix = "0x",
+                DigitGroup = 2,
+            }; } }
     }
 
     public sealed class YBigInteger
@@ -63,8 +68,15 @@ namespace Upsilon.Common.Library
 
         public YBigInteger(byte[] byteArray)
         {
+            while (byteArray.Last() == 0 && byteArray.Length > 1)
+            {
+                byteArray = byteArray.Take(byteArray.Length - 1).ToArray();
+            }
+
             this.ByteArray = byteArray;
         }
+
+        public static readonly short InternalBase = 0x100;
 
         public YBigInteger(string strValue)
         {
@@ -101,7 +113,7 @@ namespace Upsilon.Common.Library
 
         public static YBigInteger operator +(YBigInteger value1, YBigInteger value2)
         {
-            byte[] result = new byte[Math.Max(value1.ByteArray.Length, value2.ByteArray.Length)];
+            byte[] result = new byte[Math.Max(value1.ByteArray.Length, value2.ByteArray.Length) + 1];
             short carry = 0;
 
             for (int i = 0; i < result.Length; i++)
@@ -110,7 +122,12 @@ namespace Upsilon.Common.Library
                     + (i < value2.ByteArray.Length ? value2.ByteArray[i] : 0) 
                     + carry);
                 result[i] = (byte)carry;
-                carry = (short)(carry / 0x100);
+                carry = (short)(carry / YBigInteger.InternalBase);
+            }
+
+            while (result.Last() == 0 && result.Length > 1)
+            {
+                result = result.Take(result.Length - 1).ToArray();
             }
 
             return new YBigInteger(result);
@@ -133,7 +150,30 @@ namespace Upsilon.Common.Library
 
         public static YBigInteger operator *(YBigInteger value1, YBigInteger value2)
         {
-            return null;
+            List<byte[]> subResults = new();
+
+            for (int i = 0; i < value2.ByteArray.Length; i++)
+            {
+                int carry = 0;
+                byte[] subResult = new byte[i + 1 + value1.ByteArray.Length];
+                for (int j = 0; j < value1.ByteArray.Length; j++)
+                {
+                    carry = (int)((value1.ByteArray[j] * value2.ByteArray[i]) + carry);
+                    subResult[i + j] = (byte)carry;
+                    carry = (int)(carry / YBigInteger.InternalBase);
+                }
+                subResult[subResult.Length - 1] = (byte)carry;
+
+                subResults.Add(subResult);
+            }
+
+            byte[] result = new byte[] { 0 };
+            foreach (byte[] subResult in subResults)
+            {
+                result = (new YBigInteger(result) + new YBigInteger(subResult)).ByteArray;
+            }
+
+            return new YBigInteger(result);
         }
 
         public static YBigInteger operator *(YBigInteger value1, long value2)
