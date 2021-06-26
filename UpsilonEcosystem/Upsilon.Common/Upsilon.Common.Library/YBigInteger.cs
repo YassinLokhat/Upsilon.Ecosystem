ca0066801c0fcc3d961c161127801faf4a5fa069
yassinlokhat <yassinlokhat@gmail.com>
2021-06-26 20:18:54 +0400

    Upsilon.Common.Library - 1.0.1 - Refactoring Add and Multiply

--- UpsilonEcosystem/Upsilon.Common/Upsilon.Common.Library/YBigInteger.cs
+++ UpsilonEcosystem/Upsilon.Common/Upsilon.Common.Library/YBigInteger.cs
@@ -187,38 +187,14 @@ namespace Upsilon.Common.Library
                 throw new Exception($"'{strValue1}({base1.BaseName})' and '{strValue2}({base2.BaseName})' should be in the same base.");
             }
 
-            string result = string.Empty;
             strValue1 = new(strValue1.Reverse().ToArray());
             strValue2 = new(strValue2.Reverse().ToArray());
 
-            byte carry = 0;
+            byte[] result = YBigInteger.AddBytes(strValue1.Select(x => (byte)base1.Alphabet.IndexOf(x)).ToArray(),
+                strValue2.Select(x => (byte)base1.Alphabet.IndexOf(x)).ToArray(),
+                (byte)base1.BaseName);
 
-            for (int i = 0; i < Math.Max(strValue1.Length, strValue2.Length); i++)
-            {
-                byte digit1 = 0;
-                if (i < strValue1.Length)
-                {
-                    digit1 = (byte)base1.Alphabet.IndexOf(strValue1[i]);
-                }
-
-                byte digit2 = 0;
-                if (i < strValue2.Length)
-                {
-                    digit2 = (byte)base1.Alphabet.IndexOf(strValue2[i]);
-                }
-
-                carry = (byte)(digit1 + digit2 + carry);
-                result += base1.Alphabet[carry % (int)base1.BaseName];
-                carry = (byte)(carry / (int)base1.BaseName);
-            }
-
-            while (carry != 0)
-            {
-                result += base1.Alphabet[carry % (int)base1.BaseName];
-                carry = (byte)(carry / (int)base1.BaseName);
-            }
-
-            return base1.Prefix + new string(result.Reverse().ToArray());
+            return base1.Prefix + new string(result.Select(x => base1.Alphabet[x]).Reverse().ToArray());
         }
 
         public static string MultiplyString(string strValue1, string strValue2)
@@ -233,44 +209,12 @@ namespace Upsilon.Common.Library
 
             strValue1 = new(strValue1.Reverse().ToArray());
             strValue2 = new(strValue2.Reverse().ToArray());
-            List<string> subResults = new();
 
-            for (int i = 0; i < strValue2.Length; i++)
-            {
-                byte digit2 = (byte)base1.Alphabet.IndexOf(strValue2[i]);
+            byte[] result = YBigInteger.MultiplyBytes(strValue1.Select(x => (byte)base1.Alphabet.IndexOf(x)).ToArray(),
+                strValue2.Select(x => (byte)base1.Alphabet.IndexOf(x)).ToArray(),
+                (byte)base1.BaseName);
 
-                int carry = 0;
-                string subResult = string.Empty;
-                for (int j = 0; j < i; j++)
-                {
-                    subResult += "0";
-                }
-
-                for (int j = 0; j < strValue1.Length; j++)
-                {
-                    byte digit1 = (byte)base1.Alphabet.IndexOf(strValue1[j]);
-
-                    carry = (int)((digit1 * digit2) + carry);
-                    subResult += base1.Alphabet[carry % (int)base1.BaseName];
-                    carry = (int)(carry / (int)base1.BaseName);
-                }
-
-                while (carry != 0)
-                {
-                    subResult += base1.Alphabet[carry % (int)base1.BaseName];
-                    carry = (byte)(carry / (int)base1.BaseName);
-                }
-
-                subResults.Add(base1.Prefix + new string(subResult.Reverse().ToArray()));
-            }
-
-            string result = base1.Prefix + "0";
-            foreach (string subResult in subResults)
-            {
-                result = YBigInteger.AddString(result, subResult);
-            }
-
-            return result;
+            return base1.Prefix + new string(result.Select(x => base1.Alphabet[x]).Reverse().ToArray());
         }
 
         private static Base _getBase(ref string strValue)
@@ -294,26 +238,89 @@ namespace Upsilon.Common.Library
             return @base;
         }
 
+        public static byte[] AddBytes(byte[] value1, byte[] value2, short @base)
+        {
+            List<byte> result = new();
+
+            short carry = 0;
+            for (int i = 0; i < Math.Max(value1.Length, value2.Length); i++)
+            {
+                byte digit1 = 0;
+                if (i < value1.Length)
+                {
+                    digit1 = value1[i];
+                }
+
+                byte digit2 = 0;
+                if (i < value2.Length)
+                {
+                    digit2 = value2[i];
+                }
+
+                carry = (short)(digit1 + digit2 + carry);
+                result.Add((byte)(carry % @base));
+                carry = (short)(carry / @base);
+            }
+
+            while (carry != 0)
+            {
+                result.Add((byte)(carry % @base));
+                carry = (short)(carry / @base);
+            }
+
+            while (result.Last() == 0 && result.Count > 1)
+            {
+                result.RemoveAt(result.Count - 1);
+            }
+
+            return result.ToArray();
+        }
+
+        public static byte[] MultiplyBytes(byte[] value1, byte[] value2, short @base)
+        {
+            List<byte[]> subResults = new();
+
+            for (int i = 0; i < value2.Length; i++)
+            {
+                byte digit2 = value2[i];
+
+                int carry = 0;
+                List<byte> subResult = new();
+                for (int j = 0; j < i; j++)
+                {
+                    subResult.Add(0);
+                }
+
+                for (int j = 0; j < value1.Length; j++)
+                {
+                    byte digit1 = value1[j];
+
+                    carry = ((digit1 * digit2) + carry);
+                    subResult.Add((byte)(carry % @base));
+                    carry /= @base;
+                }
+
+                while (carry != 0)
+                {
+                    subResult.Add((byte)(carry % @base));
+                    carry /= @base;
+                }
+
+                subResults.Add(subResult.ToArray());
+            }
+
+            byte[] result = new byte[] { 0 };
+            foreach (byte[] subResult in subResults)
+            {
+                result = YBigInteger.AddBytes(result, subResult, @base);
+            }
+
+            return result;
+        }
+
         public static YBigInteger operator +(YBigInteger value1, YBigInteger value2)
         {
-            byte[] result = new byte[Math.Max(value1.ByteArray.Length, value2.ByteArray.Length) + 1];
-            short carry = 0;
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                carry = (short)((i < value1.ByteArray.Length ? value1.ByteArray[i] : 0)
-                    + (i < value2.ByteArray.Length ? value2.ByteArray[i] : 0) 
-                    + carry);
-                result[i] = (byte)carry;
-                carry = (short)(carry / YBigInteger.InternalBase);
-            }
-
-            while (result.Last() == 0 && result.Length > 1)
-            {
-                result = result.Take(result.Length - 1).ToArray();
-            }
-
-            return new YBigInteger(result);
+            return new YBigInteger(YBigInteger.AddBytes(value1.ByteArray, value2.ByteArray, 0x100));
         }
 
         public static YBigInteger operator +(YBigInteger value1, long value2)
@@ -328,30 +335,7 @@ namespace Upsilon.Common.Library
 
         public static YBigInteger operator *(YBigInteger value1, YBigInteger value2)
         {
-            List<byte[]> subResults = new();
-
-            for (int i = 0; i < value2.ByteArray.Length; i++)
-            {
-                int carry = 0;
-                byte[] subResult = new byte[i + 1 + value1.ByteArray.Length];
-                for (int j = 0; j < value1.ByteArray.Length; j++)
-                {
-                    carry = (int)((value1.ByteArray[j] * value2.ByteArray[i]) + carry);
-                    subResult[i + j] = (byte)carry;
-                    carry = (int)(carry / YBigInteger.InternalBase);
-                }
-                subResult[subResult.Length - 1] = (byte)carry;
-
-                subResults.Add(subResult);
-            }
-
-            byte[] result = new byte[] { 0 };
-            foreach (byte[] subResult in subResults)
-            {
-                result = (new YBigInteger(result) + new YBigInteger(subResult)).ByteArray;
-            }
-
-            return new YBigInteger(result);
+            return new YBigInteger(YBigInteger.MultiplyBytes(value1.ByteArray, value2.ByteArray, 0x100));
         }
 
         public static YBigInteger operator *(YBigInteger value1, long value2)
