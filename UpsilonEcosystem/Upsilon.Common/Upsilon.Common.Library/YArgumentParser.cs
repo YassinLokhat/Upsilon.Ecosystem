dd15863276db5fcd917f315e7b2d426c43180bc5
yassinlokhat <yassinlokhat@gmail.com>
2021-06-30 08:17:20 +0400

    UpsilonEcosystem - Documentation - Upsilon.Common.Library - Part 3

--- UpsilonEcosystem/Upsilon.Common/Upsilon.Common.Library/YArgumentParser.cs
+++ UpsilonEcosystem/Upsilon.Common/Upsilon.Common.Library/YArgumentParser.cs
@@ -7,27 +7,53 @@ using System.Threading.Tasks;
 namespace Upsilon.Common.Library
 {
     /// <summary>
-    /// The Upsilon Ecosystem argument parsing engine
+    /// The Upsilon Ecosystem argument parsing engine.
+    /// <seealso cref="YArgument"/>
     /// </summary>
     public class YArgumentParser
     {
         /// <summary>
-        /// Get the list of <c>YArguments</c> parsed by the engine
+        /// Get the list of <c><see cref="YArgument"/></c> parsed by the engine.
         /// </summary>
         public List<YArgument> Arguments { get; private set; }
         
         /// <summary>
-        /// Get the raw arguments given to the engine
+        /// Get the raw arguments given to the engine.
         /// </summary>
         public string[] Args { get; private set; }
 
         /// <summary>
-        /// Get the list of argument modifiers
+        /// Get the list of argument modifiers :
+        /// <list type="bullet">
+        /// <item><description>the dash '-'</description></item>
+        /// <item><description>the slash '/'</description></item>
+        /// </list>
         /// </summary>
         public static readonly char[] ArgModifiers = new[] { '-', '/' };
 
         /// <summary>
-        /// Create a new argument parser engine from an arguments list
+        /// Create a new argument parser engine from an argument list.
+        /// <example>
+        /// <code>
+        ///    YArgumentParser argumentParser = new YArgumentParser(new string[]
+        ///        { 
+        ///            "First value of the main argument",
+        ///            "Second value of the main argument",
+        ///            "-s",
+        ///            "source",
+        ///            "/d",
+        ///            "destination",
+        ///            "-readonly"
+        ///        });
+        /// </code>
+        /// In results, <c>argumentParser</c> will have 4 arguments : 
+        /// <list type="bullet">
+        /// <item><description>the main argument which has 2 values</description></item>
+        /// <item><description><c>s</c> argument which has <c>source</c> as value</description></item>
+        /// <item><description><c>d</c> argument which has <c>destination</c> as value</description></item>
+        /// <item><description><c>readonly</c> argument which is set as boolean</description></item>
+        /// </list>
+        /// </example>
         /// </summary>
         /// <param name="args">The arguments list</param>
         public YArgumentParser(string[] args)
@@ -70,11 +96,34 @@ namespace Upsilon.Common.Library
         }
 
         /// <summary>
-        /// Get a <c>YArguments</c> from the <paramref name="argName"/> or <c>null</c> if the given param is not found.
+        /// Get a <c><see cref="YArgument"/></c> from the <paramref name="argName"/> or <c>null</c> if the given param is not found.
+        /// <example>
+        /// <code>
+        ///    YArgumentParser argumentParser = new YArgumentParser(new string[]
+        ///        { 
+        ///            "First value of the main argument",
+        ///            "Second value of the main argument",
+        ///            "-s",
+        ///            "source",
+        ///            "/d",
+        ///            "destination",
+        ///            "-readonly"
+        ///        });
+        ///    YArgument argument = argumentParser.GetArgument("s");
+        ///    YArgument mainArgument = argumentParser.GetArgument(string.Empty);
+        ///    YArgument nullArgument = argumentParser.GetArgument("m");
+        /// </code>
+        /// In results :
+        /// <list type="bullet">
+        ///     <item><description><c>argument.Values</c> will be <c>{ "source" }</c></description></item>
+        ///     <item><description><c>mainArgument.Values</c> will be <c>{ "First value of the main argument", "Second value of the main argument"}</c></description></item>
+        ///     <item><description><c>nullArgument</c> will be <c>null</c></description></item>
+        /// </list>
+        /// </example>
         /// </summary>
         /// <remarks>Giving string.Empty to the <paramref name="argName"/> param will return the main <c>YArguments</c></remarks>
         /// <param name="argName"></param>
-        /// <returns>Returns the <c>YArguments</c> or <c>null</c></returns>
+        /// <returns>Returns the <c><see cref="YArgument"/></c> or <c>null</c></returns>
         public YArgument GetArgument(string argName)
         {
             return this.Arguments.Find(x => x.Name == argName);
@@ -82,6 +131,27 @@ namespace Upsilon.Common.Library
 
         /// <summary>
         /// Check if the <paramref name="argName"/> is set.
+        /// <example>
+        /// <code>
+        ///    YArgumentParser argumentParser = new YArgumentParser(new string[]
+        ///        { 
+        ///            "First value of the main argument",
+        ///            "Second value of the main argument",
+        ///            "-s",
+        ///            "source",
+        ///            "/d",
+        ///            "destination",
+        ///            "-readonly"
+        ///        });
+        /// </code>
+        /// In results :
+        /// <list type="bullet">
+        ///     <item><description><c>argumentParser.HasArgument(string.Empty)</c> will return <c>true</c> because it has main argument</description></item>
+        ///     <item><description><c>argumentParser.HasArgument("s")</c> will return <c>true</c></description></item>
+        ///     <item><description><c>argumentParser.HasArgument("readonly")</c> will return <c>true</c></description></item>
+        ///     <item><description><c>argumentParser.HasArgument("m")</c> will return <c>false</c></description></item>
+        /// </list>
+        /// </example>
         /// </summary>
         /// <param name="argName"></param>
         /// <returns>Returns <c>true</c> or <c>false</c></returns>
@@ -92,6 +162,27 @@ namespace Upsilon.Common.Library
 
         /// <summary>
         /// Check if the <paramref name="argName"/> is set as a boolean.
+        /// <example>
+        /// <code>
+        ///    YArgumentParser argumentParser = new YArgumentParser(new string[]
+        ///        { 
+        ///            "First value of the main argument",
+        ///            "Second value of the main argument",
+        ///            "-s",
+        ///            "source",
+        ///            "/d",
+        ///            "destination",
+        ///            "-readonly"
+        ///        });
+        /// </code>
+        /// In results :
+        /// <list type="bullet">
+        ///     <item><description><c>argumentParser.ArgumentIsSet(string.Empty)</c> will return <c>false</c> because the main argument cannot be set as boolean</description></item>
+        ///     <item><description><c>argumentParser.ArgumentIsSet("s")</c> will return <c>false</c> because this argument is not set as boolean</description></item>
+        ///     <item><description><c>argumentParser.ArgumentIsSet("readonly")</c> will return <c>true</c></description></item>
+        ///     <item><description><c>argumentParser.ArgumentIsSet("override")</c> will return <c>false</c></description></item>
+        /// </list>
+        /// </example>
         /// </summary>
         /// <param name="argName"></param>
         /// <returns>Returns <c>true</c> or <c>false</c></returns>
@@ -102,9 +193,9 @@ namespace Upsilon.Common.Library
         }
 
         /// <summary>
-        /// Get the main <c>YArguments</c> or <c>null</c> if not found.
+        /// Get the main <c><see cref="YArgument"/></c> or <c>null</c> if not found.
         /// </summary>
-        /// <returns>Returns the <c>YArguments</c> or <c>null</c></returns>
+        /// <returns>Returns the <c><see cref="YArgument"/></c> or <c>null</c></returns>
         public YArgument GetMainArgument()
         {
             return GetArgument(string.Empty);
@@ -117,22 +208,23 @@ namespace Upsilon.Common.Library
     }
 
     /// <summary>
-    /// This class represent an argument
+    /// This class represent an argument parsed by a <c><see cref="YArgumentParser"/></c>.
+    /// <seealso cref="YArgumentParser"/>
     /// </summary>
     public class YArgument
     {
         /// <summary>
-        /// The name of the argument without its modifier
+        /// The name of the argument without its modifier.
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// The values of the argument
+        /// The values of the argument.
         /// </summary>
         public List<string> Values { get; set; } = null;
 
         /// <summary>
-        /// Check if the argument is set as a boolean
+        /// Check if the argument is set as a boolean.
         /// </summary>
         public bool IsBoolean
         {
