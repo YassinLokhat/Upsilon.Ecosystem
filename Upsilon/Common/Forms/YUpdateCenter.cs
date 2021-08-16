a2ec097ed98a793b9684ba55982a8e4be5c8693f
Yassin Lokhat <yassinlokhat@gmail.com>
2021-08-16 15:01:56 +0400

    Upsilon.Common.Forms - YUpdateCenter.CheckForUpdate uses reflection to retrieve calling assembly info then download and install updates

--- Upsilon/Common/Forms/YUpdateCenter.cs
+++ Upsilon/Common/Forms/YUpdateCenter.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,9 +13,14 @@ namespace Upsilon.Common.Forms
 {
     public static class YUpdateCenter
     {
-        public static YAssembly CheckForUpdate(string assemblyName, YVersion localVersion, string serverUrl, string message = null, string title = null)
+        public static YAssembly CheckForUpdate(string serverUrl, string message = null, string title = null)
         {
-            YAssembly onlineAssembly = YUpdateCentre.CheckForUpdate(serverUrl, assemblyName);
+            Assembly local = Assembly.GetCallingAssembly();
+
+            string assemblyName = local.GetName().Name;
+
+            YVersion localVersion = new(local.GetName().Version);
+            Dictionary<string, List<YAssembly>> deployedAssemblies = YUpdateCentre.CheckForUpdate(serverUrl, assemblyName, out YAssembly onlineAssembly);
 
             if (String.IsNullOrWhiteSpace(message))
             {
@@ -31,13 +39,41 @@ namespace Upsilon.Common.Forms
                     && MessageBox.Show(message,
                     title, MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == DialogResult.OK)
                 {
-                    YStaticMethods.ProcessStartUrl(onlineAssembly.Url);
+                    YUpdateCenter._downloadUpdate(onlineAssembly, deployedAssemblies, local.Location);
                 }
 
                 Environment.Exit(0);
             }
 
             return onlineAssembly;
+        }
+
+        private static void _downloadUpdate(YAssembly onlineAssembly, Dictionary<string, List<YAssembly>> deployedAssemblies, string outputPath)
+        {
+            string tempDir = Path.Combine(Assembly.GetExecutingAssembly().Location, DateTime.Now.Ticks.ToString());
+
+            if (Directory.Exists(tempDir))
+            {
+                Directory.Delete(tempDir, true);
+            }
+            Directory.CreateDirectory(tempDir);
+
+            onlineAssembly.DownloadAssembly(deployedAssemblies, tempDir);
+            string[] files = Directory.GetFiles(tempDir, "*", SearchOption.AllDirectories);
+
+            string args = "timeout 5"
+                + " & move -Y"
+                + $" {string.Join(',', files.Select(x => $"\"{x}\""))}"
+                + $" {outputPath}";
+
+            ProcessStartInfo processStartInfo = new()
+            {
+                FileName = "cmd",
+                Arguments = args,
+                CreateNoWindow = true,
+            };
+
+            Process.Start(processStartInfo);
         }
     }
 }
