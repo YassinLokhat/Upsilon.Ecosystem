21c22fa5e17e3d5b40609fd7477b3a59aa1d0b14
Yassin Lokhat <yassinlokhat@gmail.com>
2021-08-19 07:15:59 +0400

    Upsilon.Common.Library - Adding YUpdateCenter.DownloadUpdate method

--- Upsilon/Common/Library/YUpdateCenter.cs
+++ Upsilon/Common/Library/YUpdateCenter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
@@ -69,6 +70,62 @@ namespace Upsilon.Common.Library
             }
 
             return deployedAssemblies;
+        }
+
+        /// <summary>
+        /// Download and update the given assembly.
+        /// </summary>
+        /// <param name="onlineAssembly">The new version of the assembly.</param>
+        /// <param name="deployedAssemblies">The deployed assemblies list.</param>
+        /// <param name="outputPath">The directory where the assembly to update binaries are. By default it will be the location of the calling assembly.</param>
+        public static void DownloadUpdate(YAssembly onlineAssembly, Dictionary<string, List<YAssembly>> deployedAssemblies, string outputPath = null)
+        {
+            if (string.IsNullOrWhiteSpace(outputPath))
+            {
+                outputPath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+            }
+
+            string tempDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), DateTime.Now.Ticks.ToString());
+            tempDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "update");
+
+            if (Directory.Exists(tempDir))
+            {
+                Directory.Delete(tempDir, true);
+            }
+            Directory.CreateDirectory(tempDir);
+
+            onlineAssembly.DownloadAssembly(deployedAssemblies, tempDir);
+            var newFiles = Directory.GetFiles(tempDir, "*", SearchOption.AllDirectories);
+            var oldFiles = newFiles.Select(x => x.Replace(tempDir, outputPath)).ToArray();
+
+            string args = "/C timeout 4";
+
+            string delCommand = "";
+            string moveCommand = "";
+            for (int i = 0; i < newFiles.Length; i++)
+            {
+                delCommand += $" & del /F /Q \"{oldFiles[i]}\"";
+                moveCommand += $" & move /Y \"{newFiles[i]}\" \"{oldFiles[i]}\"";
+            }
+
+            args += delCommand
+                + moveCommand
+                + $" & rmdir \"{tempDir}\" /S /Q";
+
+            if (onlineAssembly.BinaryType != YBinaryType.ClassLibrary)
+            {
+                args += $" & \"{Path.Combine(outputPath, onlineAssembly.Name + ".exe")}\"";
+            }
+
+            ProcessStartInfo processStartInfo = new()
+            {
+                FileName = "cmd",
+                Arguments = args,
+                CreateNoWindow = true,
+            };
+
+            Process.Start(processStartInfo);
+            Environment.Exit(0);
         }
     }
 }
