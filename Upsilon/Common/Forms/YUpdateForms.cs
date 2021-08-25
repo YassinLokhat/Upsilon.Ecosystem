b0d0a5bbbe3c043ead46898001843a46775917f0
Yassin Lokhat <yassinlokhat@gmail.com>
2021-08-25 08:50:12 +0400

    UpsilonEcosystem.Documentation - Upsilon.Common.Forms - Part 2

--- Upsilon/Common/Forms/YUpdateForms.cs
+++ Upsilon/Common/Forms/YUpdateForms.cs
@@ -12,19 +12,19 @@ using Upsilon.Common.Library;
 namespace Upsilon.Common.Forms
 {
     /// <summary>
-    /// A stati class giving some updating methods.
+    /// A static class giving some updating methods.
     /// </summary>
     public static class YUpdateForms
     {
         /// <summary>
-        /// Check if the given assembly has a new version.
+        /// Download and update the given assembly if available.
         /// </summary>
-        /// <param name="serverUrl"></param>
-        /// <param name="assemblyName"></param>
-        /// <param name="localVersion"></param>
-        /// <param name="message"></param>
-        /// <param name="title"></param>
-        /// <returns></returns>
+        /// <param name="serverUrl">The deployed assemblies json url.</param>
+        /// <param name="assemblyName">The name of the assembly to update.</param>
+        /// <param name="localVersion">The current version of the assembly.</param>
+        /// <param name="message">The message to show when new version is available.</param>
+        /// <param name="title">The title of the message box.</param>
+        /// <returns>Return the lastest <c><see cref="YAssembly"/></c>.</returns>
         public static YAssembly CheckForUpdate(string serverUrl, string assemblyName, YVersion localVersion, string message = null, string title = null)
         {
             Dictionary<string, List<YAssembly>> deployedAssemblies = YUpdateCenter.CheckForUpdate(serverUrl, assemblyName, out YAssembly onlineAssembly);
@@ -55,6 +55,13 @@ namespace Upsilon.Common.Forms
             return onlineAssembly;
         }
 
+        /// <summary>
+        /// Download and update the calling assembly if available.
+        /// </summary>
+        /// <param name="serverUrl">The deployed assemblies json url.</param>
+        /// <param name="message">The message to show when new version is available.</param>
+        /// <param name="title">The title of the message box.</param>
+        /// <returns>Return the lastest <c><see cref="YAssembly"/></c>.</returns>
         public static YAssembly CheckForUpdate(string serverUrl, string message = null, string title = null)
         {
             Assembly local = Assembly.GetCallingAssembly();
